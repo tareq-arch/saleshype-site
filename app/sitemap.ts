1785b8af834870a157f5888af2f79089b1248b3d
tareq-arch <tareq@saleshype.co.uk>
2026-05-21 14:01:03 +0100

    Add new SEO service pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,10 +5,14 @@ const baseUrl = "https://www.saleshype.co.uk";
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
     "",
+    "/b2b-lead-generation-services",
     "/b2b-outbound-lead-generation-uk",
+    "/outsourced-lead-generation",
     "/managed-cold-email-uk",
+    "/cold-email-agency",
     "/cold-email-agency-uk",
     "/apollo-hubspot-outbound",
+    "/hubspot-solutions-partner-uk",
     "/gcc-lead-generation",
     "/cold-email-deliverability-checklist",
   ];
